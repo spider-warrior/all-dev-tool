cdcd869effe05b9cf01997a084e821db50633c15
amen <362961910@qq.com>
2020-02-17 23:21:49 +0800

    sock5 proxy 优化

--- net-proxy-tool/src/main/java/cn/t/tool/netproxytool/socks5/Socks5ProxyServerDaemon.java
+++ net-proxy-tool/src/main/java/cn/t/tool/netproxytool/socks5/Socks5ProxyServerDaemon.java
@@ -5,7 +5,7 @@ package cn.t.tool.netproxytool.socks5;
  * @version V1.0
  * @since 2020-02-17 23:14
  **/
-public class Socks5ProxyServerDeamon {
+public class Socks5ProxyServerDaemon {
     public static void main(String[] args) {
         Socks5ProxyServer server = new Socks5ProxyServer(10086,100, "106.13.61.214");
         new Thread(server).start();
