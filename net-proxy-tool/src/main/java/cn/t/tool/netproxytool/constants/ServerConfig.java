2be845c6aec4e03e34ca4dc9ea76d73d3105f4a4
amen <362961910@qq.com>
2020-02-23 01:59:12 +0800

    代理优化

--- net-proxy-tool/src/main/java/cn/t/tool/netproxytool/constants/ServerConfig.java
+++ net-proxy-tool/src/main/java/cn/t/tool/netproxytool/constants/ServerConfig.java
@@ -15,7 +15,8 @@ public class ServerConfig {
     public static final String SERVER_HOST = SystemUtil.getLocalIp();
     public static final byte[] SERVER_HOST_BYTES = SystemUtil.convertHostToBytes(SERVER_HOST);
     public static final short PORT = 10086;
-    public static final int CORE_THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+    public static final int PROCESSOR_COUNT = Runtime.getRuntime().availableProcessors();
+    public static final int CORE_THREAD_COUNT = PROCESSOR_COUNT < 4 ? 2 : PROCESSOR_COUNT;
     public static final int MAX_THREAD_COUNT = CORE_THREAD_COUNT * 5;
     public static final int BLOCKING_THREAD_COUNT = MAX_THREAD_COUNT;
     public static final int THREAD_TT = 10;
