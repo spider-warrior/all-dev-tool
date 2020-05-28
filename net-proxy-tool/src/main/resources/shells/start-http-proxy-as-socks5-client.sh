f8f92e69de67e93dbb882742378926834e387cb1
yangjian <yangjian@ishansong.com>
2020-05-28 12:29:23 +0800

    补充shell脚本

---
+++ net-proxy-tool/src/main/resources/shells/start-http-proxy-as-socks5-client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp net-proxy.jar cn.t.tool.netproxytool.http.HttpProxyServerViaSocks5 127.0.0.1:10086 admin:123456 gEIZCLU+48tkSknFfuE9kBCuKyhOrwnt54mJ3KX+uWE=
---
+++ net-proxy-tool/src/main/resources/shells/start-socks5-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar net-proxy.jar
