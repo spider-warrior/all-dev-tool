2c89e1638f85ccf1676906220a1f3412b3e74f8c
yangjian <362961910@qq.com>
2020-08-30 23:55:11 +0800

    update thread pool config

--- net-proxy-tool/src/main/java/cn/t/tool/netproxytool/constants/ThreadPoolConfig.java
+++ net-proxy-tool/src/main/java/cn/t/tool/netproxytool/constants/ThreadPoolConfig.java
@@ -10,8 +10,8 @@ import java.util.concurrent.TimeUnit;
 public class ThreadPoolConfig {
     public static final String PROXY_THREAD_POOL_NAME = "net-proxy";
     public static final int PROCESSOR_COUNT = Runtime.getRuntime().availableProcessors();
-    public static final int PROXY_CORE_THREAD_COUNT = (PROCESSOR_COUNT < 4 ? 2 : PROCESSOR_COUNT) * 6;
-    public static final int PROXY_BLOCKING_THREAD_COUNT = PROXY_CORE_THREAD_COUNT / 3;
+    public static final int PROXY_CORE_THREAD_COUNT = (PROCESSOR_COUNT < 4 ? 2 : PROCESSOR_COUNT) * 4;
+    public static final int PROXY_BLOCKING_THREAD_COUNT = 0;
     public static final int PROXY_MAX_THREAD_COUNT = (PROXY_CORE_THREAD_COUNT + PROXY_BLOCKING_THREAD_COUNT) * 2;
     public static final int PROXY_THREAD_TT = 10;
     public static final TimeUnit PROXY_THREAD_TT_TIME_UNIT = TimeUnit.SECONDS;
